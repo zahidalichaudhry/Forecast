87aa2a99836d3f40858da2430fd42ea4bfac2f39
ansarali70 <ansar.ali4018@gmail.com>
2018-01-09 12:43:23 +0500

    Ansar

---
+++ app/src/main/java/com/example/zahidali/forecast_final/NaviDrawer.java
@@ -0,0 +1,13 @@
+package com.example.zahidali.forecast_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NaviDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi_drawer);
+    }
+}
---
+++ app/src/main/java/com/example/zahidali/forecast_final/Splash_Screen.java
@@ -0,0 +1,33 @@
+package com.example.zahidali.forecast_final;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.zahidali.forecast_final.Activity.MainActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        imageView = (ImageView)findViewById(R.id.imgView);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
